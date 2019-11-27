f33e4b944c28c67603e0fb661565d4f8ddf9bcac
jlu <jlu@B212-38>
2019-11-27 10:14:32 +0800

    添加项目文件。

---
+++ JLUCG05/JLUCG05View.cpp
@@ -0,0 +1,104 @@
+﻿
+// JLUCG05View.cpp: CJLUCG05View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "JLUCG05.h"
+#endif
+
+#include "JLUCG05Doc.h"
+#include "JLUCG05View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CJLUCG05View
+
+IMPLEMENT_DYNCREATE(CJLUCG05View, CView)
+
+BEGIN_MESSAGE_MAP(CJLUCG05View, CView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CJLUCG05View 构造/析构
+
+CJLUCG05View::CJLUCG05View() noexcept
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CJLUCG05View::~CJLUCG05View()
+{
+}
+
+BOOL CJLUCG05View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CJLUCG05View 绘图
+
+void CJLUCG05View::OnDraw(CDC* /*pDC*/)
+{
+	CJLUCG05Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CJLUCG05View 打印
+
+BOOL CJLUCG05View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CJLUCG05View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CJLUCG05View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+
+// CJLUCG05View 诊断
+
+#ifdef _DEBUG
+void CJLUCG05View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CJLUCG05View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CJLUCG05Doc* CJLUCG05View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CJLUCG05Doc)));
+	return (CJLUCG05Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CJLUCG05View 消息处理程序
---
+++ JLUCG05/JLUCG05View.h
@@ -0,0 +1,49 @@
+﻿
+// JLUCG05View.h: CJLUCG05View 类的接口
+//
+
+#pragma once
+
+
+class CJLUCG05View : public CView
+{
+protected: // 仅从序列化创建
+	CJLUCG05View() noexcept;
+	DECLARE_DYNCREATE(CJLUCG05View)
+
+// 特性
+public:
+	CJLUCG05Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CJLUCG05View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // JLUCG05View.cpp 中的调试版本
+inline CJLUCG05Doc* CJLUCG05View::GetDocument() const
+   { return reinterpret_cast<CJLUCG05Doc*>(m_pDocument); }
+#endif
+
